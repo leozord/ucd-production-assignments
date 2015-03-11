fde7993ef3e5a4f30427a20a2c627ab75c3e8071
elerafa <leonardo.rafaeli@ericsson.com>
2015-03-11 15:39:14 +0000

    added first scratch of  addressbook

---
+++ proto-grpc/client-runner.py
@@ -0,0 +1,12 @@
+__author__ = 'elerafa'
+
+import addressbook_pb2
+person = addressbook_pb2.Person()
+person.id = 1234
+person.name = "John Doe"
+person.email = "jdoe@example.com"
+phone = person.phone.add()
+phone.number = "555-4321"
+phone.type = addressbook_pb2.Person.HOME
+
+print(person.SerializeToString())
